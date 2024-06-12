a8aada0ddd308790262d640f19b02755e007246f
unknown <Administrator@ECM-IMG.CL>
2024-06-12 11:25:10 -0400

    primer commit

---
+++ assets/js/inmobiliaria.js
@@ -0,0 +1,76 @@
+const propiedades_alquiler = [
+    {
+      id: "1",
+      nombre: "Casa en Irlanda",
+      src: "./assets/img/casairlanda.jpg",
+      descripcion:
+        "Casa en medio de los parajes de Irlanda.",
+      ubicacion: "Bosque Irlandes",
+      habitaciones: 3,
+      costo: 1500,
+      pets: true,
+      smoke: false,
+    },
+    {
+      id: "2",
+      nombre: "Apartamento urbano",
+      src: "./assets/img/casafrancia.jpg",
+      descripcion:
+        "Casa a las afuera de Paris.",
+      ubicacion: "Rui de Rivoli, 1023",
+      habitaciones: 2,
+      costo: 1000,
+      pets: false,
+      smoke: false,
+    },
+    {
+      id: "3",
+      nombre: "Casa en Inglaterra",
+      src: "./assets/img/casainglaterra.jpg",
+      descripcion:
+        "Casa en Inglaterra.",
+      ubicacion: "united kingdom, 432",
+      habitaciones: 1,
+      costo: 800,
+      pets: true,
+      smoke: false,
+    },
+  ];
+  
+  const propiedades_venta = [
+    {
+      id: "7",
+      nombre: "Casa Residencial",
+      src: "./assets/img/casapiscina.jpg",
+      descripcion:
+        "Impresionante Casa con vista al mar.",
+      ubicacion: "Con Con, 693",
+      habitaciones: 8,
+      costo: 6000, 
+      pets: true,
+      smoke: false,
+    },
+    {
+      id: "8",
+      nombre: "Casa en Vitacura",
+      src: "./assets/img/casavitacura.jpg",
+      descripcion: "Casa residencial con las mejores ubicaciones del sector.",
+      ubicacion: "El Arrayan, 834",
+      habitaciones: 8,
+      costo: 4800, 
+      pets: false,
+      smoke: true,
+    },
+    {
+      id: "9",
+      nombre: "Casa Panoramica",
+      src: "./assets/img/casapiscina2.jpg",
+      descripcion: "Casa con vista panoramica a la ciudad.",
+      ubicacion: "Washington, DC",
+      habitaciones: 8,
+      costo: 4000, 
+      pets: true,
+      smoke: true,
+    },
+ 
+  ];
